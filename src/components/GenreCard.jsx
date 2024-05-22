b2e0b66e723fadaf904bb3404e391054e993d006
Patrickodida <patrick.o.odida@gmail.com>
2024-05-22 15:33:00 +0300

    Add the movie genre section

---
+++ src/components/GenreCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function GenreCard(){
+    return (
+        <div>
+            <section className="movie-genre">
+                <img src="" alt="" />
+                <h2>Action</h2>
+            </section>
+        </div>
+    )
+}
+export default GenreCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import homeImage from '../images/ghostbusters-action-poster.webp'
+import homeImage from '../images/ghostbusters-action-poster.webp';
+import GenreCard from "../components/GenreCard";
 
 function Home(){
     return (
@@ -12,6 +13,8 @@ function Home(){
                 description="Welcome to our curated collection of top-rated films, where every movie love can find something to cherish. our comprehensive rankings and reviews are here to guide you through the mesmerizing world of cinema."
                 image={homeImage}
              />
+             <h1 className="text-[#f2f2f5]">Movie Genres</h1>
+             <GenreCard />
         </div>
     )
 }
