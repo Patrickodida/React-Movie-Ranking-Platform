44977ab8a2479aefd73979dcfe8b814ce0d7597a
Patrickodida <patrick.o.odida@gmail.com>
2024-05-22 08:14:06 +0300

    Modify the navbar and make it responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,23 +9,25 @@ function Navbar(){
     }
 
     return (
-        <div className="bg-[#720455] text-[#f2f2f2] ">
-            <section className="flex justify-between w-[90%] m-auto m-w-[1280px] items-center  ">
-                <h1 className="logo text-[3rem] font-[900] m-0">MRP</h1>
+        <div className="bg-[#720455] text-[#f2f2f2] fixed left-0 top-0 right-0 z-50 w-full ">
+            <section className="relative flex justify-between w-[90%] m-auto m-w-[1280px] items-center   ">
+                <Link to="/" className="font-[900] text-[3rem]">
+                    <span>MRP</span>
+                </Link>
                 <div className="flex items-center w-full justify-end md:hidden">
                     <Link onClick={toggleMenu} className="text-3xl">
                     <i class={`bx ${menuOpen ? 'bx-x font-bold' : 'bx-menu font-bold' }`}></i>
                     </Link>
                 </div>
-                <nav className="nav-links flex justify-between w-full text-[0.875rem] uppercase">
-                    <ul className="primary-nav flex m-auto">
-                        <li className="mr-4">
+                <nav className={`absolute md:relative md:left-auto md:top-auto bg-[#720455] md:bg-transparent block md:flex flex-col md:flex-row md:items-center md:w-full top-10 md:top-5 right-0 left-0  pt-5 md:pt-0 pb-5 md:pb-0 mt-5 md:mt-0 ${menuOpen ? 'block' : 'hidden'} md:flex transition-transform duration-300 ${menuOpen ? 'translate-x-0' : 'translate-x-full'} md:translate-x-0 uppercase `}>
+                    <ul className="primary-nav flex flex-col md:flex-row m-auto font-bold text-center">
+                        <li className="md:mr-4 mb-4 md:mb-0">
                             <Link to="/" className="">Home</Link>
                         </li>
-                        <li className="mr-4">
+                        <li className="md:mr-4 mb-4 md:mb-0">
                             <Link to="/" className="">Genres</Link>
                         </li>
-                        <li className="mr-4">
+                        <li className="md:mr-4 mb-4 md:mb-0">
                             <Link to="/" className="">Favourites</Link>
                         </li>
                         <li>
@@ -33,11 +35,11 @@ function Navbar(){
                         </li> 
                     </ul>
 
-                    <ul className="secondary-nav flex items-center">
-                        <li className="mr-4">
+                    <ul className="secondary-nav flex flex-col md:flex-row items-center justify-center text-center mt-8 md:mt-0">
+                        <li className="md:mr-4 mb-4 md:mb-0">
                             <Link to="/">Login</Link>
                         </li>
-                        <li className="py-2 px-4 bg-[#4a0654] rounded-r-lg ">
+                        <li className="py-2 px-2 bg-[#4a0654] rounded-r-lg ">
                             <Link to="/" className="">Sign Up</Link>
                         </li>
                     </ul>
