d3ae367dc560b0df9dce53c09787bab461eef457
Patrickodida <patrick.o.odida@gmail.com>
2024-05-23 19:41:59 +0300

    Add the font-size of the genre movie title

--- src/components/GenreCard.jsx
+++ src/components/GenreCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function GenreCard({ genre, image }){
     return (
         <div>
-            <section className="movie-genre text-center bg-[#212846] rounded-lg">
+            <section className="movie-genre text-center bg-[#212846] rounded-lg text-[1.25rem] font-bold">
                 <img src={image} alt="" className="w-full rounded-t-lg" />
-                <h2 className="pb-8 pt-4">{genre}</h2>
+                <h2 className="pb-8 pt-6">{genre}</h2>
             </section>
         </div>
     )
