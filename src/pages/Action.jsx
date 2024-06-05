de6307d8e434120f31eb6a0fe1dfbcfce4a1dfc1
Patrickodida <patrick.o.odida@gmail.com>
2024-06-05 11:37:16 +0300

    Add the action page

--- src/pages/Action.jsx
+++ src/pages/Action.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 function Action(){
-    return (
-        <div>
 
+    return (
+        <div id="action" className="bg-[#030637] text-[#f2f2f5]">
+           <Navbar />
+           <Hero />
+           <Footer /> 
         </div>
     )
 }
